a9da404e6001ceaa8a5a792d15ff38ab766ec658
Utkarsh singh tiwari <129392805+Utkarsh99999@users.noreply.github.com>
2023-08-17 13:42:19 +0530

    Update Projects.jsx

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -19,14 +19,14 @@ const projectVariant = {
 
 const Project = ({ title ,Description }) => {
   const overlayStyles = `transition duration-10 bg-red flex flex-col justify-center 
-  items-center text-center md:h-[350px] w-full h-[350px] max-w-[400px] md:max-w-[600px] text-deep-blue`;
+  items-center text-center md:h-[350px] w-full max-h-[350px] max-w-[400px] md:max-w-[600px] text-deep-blue`;
   const projectTitle = title.split(" ").join("-").toLowerCase();
   const [flip,SetFlip] = useState(false);
   return (
     <motion.div variants={projectVariant} className="relative">
       <ReactCardFlip isFlipped={flip} flipDirection="vertical">
       <div className="relative">
-      <img src={`../assets/${projectTitle}.jpeg`} alt={projectTitle} />
+      <img src={`../Projects/${title}.jpeg`} alt={projectTitle} />
       <button onClick={()=>{SetFlip(!flip)}} className="absolute bottom-5 left-28 p-2
        text-deep-blue border border-solid bg-red" >Project Description</button>
       </div>
@@ -100,12 +100,12 @@ const Projects = () => {
             BEAUTIFUL USER INTERFACES
           </div>
 
-          <Project title="Project 1" Description={Description.d1} />
-          <Project title="Project 2" Description={Description.d2} />
+          <Project title="project-1" Description={Description.d1} />
+          <Project title="project-2" Description={Description.d2} />
 
           {/* ROW 2 */}
-          <Project title="Project 3" Description={Description.d3}/>
-          <Project title="Project 4"Description={Description.d4}/>
+          <Project title="project-3" Description={Description.d3}/>
+          <Project title="project-4"Description={Description.d4}/>
           <div className="flex justify-center text-center items-center p-10 bg-blue
               max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
           >
