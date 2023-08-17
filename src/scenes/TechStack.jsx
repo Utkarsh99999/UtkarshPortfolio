18e94eb98f7825d35ab38e81b092c092e0418a4b
Utkarsh singh tiwari <129392805+Utkarsh99999@users.noreply.github.com>
2023-08-17 15:23:24 +0530

    Update TechStack.jsx

--- src/scenes/TechStack.jsx
+++ src/scenes/TechStack.jsx
@@ -18,10 +18,7 @@ import img15 from '../techstack/reduxjs.png';
 import img16 from '../techstack/vscode.png';
 
 const techstack1 = [
-  img1,img2,img3,img4,img5,img6,img7,img8
-]
-const techstack2 =[
-  img9,img10,img12,img13,img14,img15,img16,img17
+  img1,img2,img3,img4,img5,img6,img7,img8,img9,img10,img12,img13,img14,img15,img16,img17
 ]
 
 const Testimonials = () => {
@@ -56,35 +53,10 @@ const Testimonials = () => {
             visible: { opacity: 1, x: 0 },
           }}
         >
-      <div className="md:flex md:justify-between bg-blue gap-8 md:gap-8 mt-10 p-10">
+      <div className="flex justify-between bg-blue gap-8 md:gap-8 mt-10 p-10 flex-wrap">
       {techstack1.map((val,index)=>{
         return (
           <motion.div
-          className="mx-auto relativ flex flex-col justify-center"
-          initial="hidden" whileInView="visible" viewport={{ once: true, amount: 0.5 }}
-          transition={{ duration: 1 }} variants={{ hidden: { opacity: 0, scale: 0.5 },
-            visible: { opacity: 1, scale: 1 }}}>
-          <img key={index} src={val} alt={val} style={{width:"100px",height:'auto'}} />
-        </motion.div>
-       )})}
-      </div>
-      </motion.div>
-
-
-      <motion.div
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true, amount: 0.5 }}
-          transition={{ duration: 0.5 }}
-          variants={{
-            hidden: { opacity: 0, x: 100 },
-            visible: { opacity: 1, x: 0 },
-          }}
-        >
-      <div className="md:flex md:justify-between bg-blue gap-8 mt-10 p-10">
-      {techstack2.map((val,index)=>{
-        return (
-        <motion.div
           className="mx-auto relativ flex flex-col justify-center"
           initial="hidden" whileInView="visible" viewport={{ once: true, amount: 0.5 }}
           transition={{ duration: 1 }} variants={{ hidden: { opacity: 0, scale: 0.5 },
